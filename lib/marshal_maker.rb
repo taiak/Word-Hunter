a88b6e07aa0d3a222d94cd2aef6239dd068012d1
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2018-04-11 06:29:41 +0300

    Update marshal_maker.rb

--- lib/marshal_maker.rb
+++ lib/marshal_maker.rb
@@ -1,7 +1,7 @@
-# if no file name exist with otomat name. name new 
+# if no file name exist with otomat name. name new
 way = File.dirname(__FILE__)
 
-Dir.chdir( way )
+Dir.chdir(way)
 Dir.mkdir('./marshalled_files') unless Dir.exist?('marshalled_files')
 system 'ruby ./automat_maker.rb'
 system 'ruby ./archive_maker.rb'

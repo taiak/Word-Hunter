9594a1ffb559c05845cf9898fe2fd16af9f091bc
Taha Yasir Kıroğlu <yasir.kiroglu@bil.omu.edu.tr>
2018-04-11 06:24:09 +0300

    Update DFA.rb

--- lib/modules/DFA.rb
+++ lib/modules/DFA.rb
@@ -1,5 +1,5 @@
 # https://github.com/andrewytliu/automata/blob/master/dfa.rb
-# DFA sınıfı yukarıdaki adresten üzerinde ufak değişiklikler yapılarak alınmıştır
+# DFA sinifi yukaridaki adresten uzerinde ufak degisiklikler yapilarak alinmistir
 class DFA
   attr_accessor :start, :end, :transition
 
@@ -11,10 +11,10 @@ class DFA
 
   def run(input)
     current_state = @start
-	    for i in input
-	    	return false unless @transition[current_state]
-	      current_state = @transition[current_state][i]
-	    end
-    return (@end.include? current_state)? true: false
+    for i in input
+      return false unless @transition[current_state]
+      current_state = @transition[current_state][i]
+     end
+    @end.include? current_state ? true : false
   end
 end
